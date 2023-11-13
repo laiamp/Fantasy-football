beb17a48e8664bbe68c3a7716a9d4447cae245e1
lmp315 <laiamogasp@gmail.com>
2023-11-13 17:17:50 +0100

    create file

---
+++ exh.cc
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+ifstream nom_arxiu_entrada("file.txt") // input file stream
+in >> var;
+nom_arxiu_entrada.close();
+
+ofstream nom_arxiu_sortida("file.txt") 
+out << var;
+nom_arxiu_sortida.close()
+/*
+constraints:
+- maxim total de T euros
+- maxim J euros per jugador
+- n1 defenses, n2 migcampistes i n3 davanters
+- 1 <= n1, n2, n3 <= 5
+- n1 + n2 + n3 = 10
+- 1 porter
+*/
+
+int main(){
+
+}
