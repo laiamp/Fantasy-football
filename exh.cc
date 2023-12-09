6fe7355ba04dca092791438658dca9a776b656e6
lmp315 <laiamogasp@gmail.com>
2023-12-09 20:58:40 +0100

    ns si he canviat algo xd

--- exh.cc
+++ exh.cc
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <chrono>
 #include <cstring>
+#include <iomanip>
 
 using namespace std;
 
@@ -43,16 +44,18 @@ void write_result(const vp& alineacio, const long long int& points, const long l
     
     ofstream out(OUTPUT_FILE);
 
-    auto end = chrono::high_resolution_clock::now();
-    auto time = chrono::duration_cast<std::chrono::seconds>(end - start);
-    out << time.count() << endl;
-
     unordered_map <string, vector<Player>> players_pos = get_players_pos(alineacio);   
     vector <string> positions = {"por", "def", "mig", "dav"};
 
+    chrono::high_resolution_clock::time_point end = chrono::high_resolution_clock::now();
+    chrono::duration<double> duration = end - start;
+    double time = duration.count();
+    out << fixed << setprecision(1) << time << endl;
+
     for (string pos: positions){
-        for (char c: pos) out << toupper(c); 
-        out << ": ";
+        string pos_upper = pos;
+        transform(pos_upper.begin(), pos_upper.end(), pos_upper.begin(), ::toupper);
+        out << pos_upper << ": "; 
         for (int i = 0; i < int(players_pos[pos].size()); i++){
             out << (i == 0 ? "" : ";") << players_pos[pos][i].name;
         }
@@ -67,23 +70,40 @@ void write_result(const vp& alineacio, const long long int& points, const long l
 
 /*
 PODES:
-    - No rebutjar si N_i - index  0 --> he de precalcular quants jugadors hi ha de cada posicio
+    - No rebutjar si N_i --> he de precalcular quants jugadors hi ha de cada posicio
     - Precalcular el mínim d'euros per posició (o els n_i de menys euros) amb una pque
-    - Comptar el total de cada posició i els count de visitats
-
-    - Faig una array mida 450. A cada posició hi haurà 4 vectors.
 */
 void check_alineacio(const vp& alineacio){
     for (Player p: alineacio) cout << p.name <<" "<< p.price <<" " << p.points << endl;
 }
 
-bool promising(int i, unordered_map <string, int> n){
-    return true;
+
+bool candidate(int i, unordered_map <string, int> n, const long long int & points, const long long int& total,
+                const long long int& max_points){
+    /*returns whether or not exists a promising solution picking the i-th player*/
+    Player player = PLAYERS[i];
+    if (n[player.pos] == 0) return false;
+    if (total + player.price > T) return false;
+
+    int k = n["por"] + n["def"] + n["mig"] + n["dav"];
+    if (i + k >= int(PLAYERS.size())) return false;
+
+    //if with the current player and the next k-1 players we dont reach max_points, we discard the candidate
+    long long int potential_points = points;
+    for (int j = i; j < i+k; j++){
+        potential_points += PLAYERS[j].points;
+    }
+    if (potential_points <= max_points) return false;
+
+    //afegir condició relacionada amb el preu
+    return true; 
 }
+
 
 void generar_alineacio(int i, vp& alineacio, unordered_map <string, int> n, long long int total,
                         long long int points, long long int& max_points,  
-                        unordered_map<string, long long int>& m_min_price){
+                        unordered_map<string, long long int>& m_min_price,
+                        unordered_map <string, int> unvisited){
     /*
         i: index jugador de les dades
         alineacio: jugadors escollits
@@ -92,34 +112,32 @@ void generar_alineacio(int i, vp& alineacio, unordered_map <string, int> n, long
         points: suma dels punts de tots els jugadors de l'alineació que portem fins ara
         max_points: maxim de punts que hem aconseguit en totes les alineacions generades
         m_min_price:
+        unvisited: 
 
-        create map unvisited
     */
-    //check_alineacio(alineacio);
+    
+    Player player = PLAYERS[i];
+
     if (11 == int(alineacio.size()) and n["por"] == 0 and n["def"] == 0 and n["mig"] == 0 and n["dav"] == 0){ // totes les posicions assignades
-        //
+        
         if (points > max_points){
             max_points = points;
             write_result(alineacio, points, total);
         }
     }
 
-    else if (i + n["por"] + n["def"] + n["mig"] + n["dav"] - 1 < int(PLAYERS.size())){
+    else if (i < int(PLAYERS.size()) and n[player.pos] <= unvisited[player.pos]){
         
-        Player player = PLAYERS[i];
-        
-        long long int points_estimats = points + player.points*(n["por"] + n["def"] + n["mig"] + n["dav"]);
-        
-    
-        if (n[player.pos] > 0 and total + player.price <= T and points_estimats >= max_points){
+        unvisited[player.pos]--;
+            
+        if (candidate(i, n, points, total, max_points)){
             n[player.pos]--;
             alineacio.push_back(player);
-            generar_alineacio(i+1, alineacio, n, total + player.price, points + player.points, max_points, m_min_price);
+            generar_alineacio(i+1, alineacio, n, total + player.price, points + player.points, max_points, m_min_price, unvisited);
             n[player.pos]++;
             alineacio.pop_back();
         }
-        
-        generar_alineacio(i+1, alineacio, n, total, points, max_points, m_min_price);
+        generar_alineacio(i+1, alineacio, n, total, points, max_points, m_min_price, unvisited);
     }
 }
 
@@ -143,7 +161,7 @@ vp get_players_from_data(string data_file, unordered_map <string, long long int>
         if (price <= J){
             Player player = {name, position, price, club, p};
             players.push_back(player);
-            min_price[player.pos] = min(player.price, min_price[player.pos]);
+            min_price[position] = min(price, min_price[position]);
             unvisited[position]++;
         }
     }
@@ -153,6 +171,7 @@ vp get_players_from_data(string data_file, unordered_map <string, long long int>
 }
 
 bool comp(const Player& a, const Player& b) {
+    /*auxiliar function to sort the players depending on their points*/
     if(a.points == b.points) return a.price > b.price;
     return a.points > b.points;
 }
@@ -188,7 +207,7 @@ int main(int argc, char** argv){
     
     long long int max_points = 0;
     
-    generar_alineacio(0, alineacio, n, 0, 0, max_points, m_min_price);
+    generar_alineacio(0, alineacio, n, 0, 0, max_points, m_min_price, unvisited);
     
     query.close();
 }
