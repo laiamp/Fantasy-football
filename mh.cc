7a445f7a39f8199fb5409167ec73f9c490de2034
lmp315 <laiamogasp@gmail.com>
2023-12-13 12:22:23 +0100

    idees

--- mh.cc
+++ mh.cc
@@ -40,7 +40,7 @@ endwhile
 
 
 greedy:
-- agrupem jugadors per posicio i filtrem els que tinguin un preu > J
+- agrupemgit  jugadors per posicio i filtrem els que tinguin un preu > J
 - per cada posicio, els agrupem en k grups segons preus (default k = 3)
 - dins de cada grup per preu, els ordenem descendentment segons puntuacio
 - fem totes les combinacions m_1, ... , m_k tals que sum(m_i) = N (n1, n2 o n3 segons posicio)
